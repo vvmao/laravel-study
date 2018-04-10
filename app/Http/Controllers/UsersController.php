639dc26c0d6a833e37cbc824c14c46154d065b4c
vvmao <1046562419@qq.com>
2018-04-10 21:55:26 +0800

    finish we signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,13 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
+
 class UsersController extends Controller
 {
+    /**
+     * 用户注册页面方法
+     * @return html
+     */
     public function create()
     {
         return view('users.create');
     }
+    /**
+     * 用户展示的方法
+     * @param  User   $user 用户实例
+     * @return html
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+    /**
+     * 用户注册的响应方法
+     * @param  Request $request 请求实例
+     * @return ''
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'email'    => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6',
+        ]);
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => $request->password,
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$user]);
+    }
+
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,8 +9,10 @@
 </head>
 
 <body>
+
     @include('layouts._header')
     <div class="container">
+    	@include('shared._messages')
         @yield('content')
 	    @include('layouts._footer')
     </div>
